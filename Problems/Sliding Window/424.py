8e4a44878a747a0fc6c80d4540dea389e2115518
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-06-30 22:03:18 -0400

    6/30/24

--- Problems/Sliding Window/424.py
+++ Problems/Sliding Window/424.py
@@ -29,8 +29,23 @@ class Solution:
                 l += 1
             maxLen = max(maxLen, r - l + 1)
         return maxLen
+# Redone 6/30/24 | O(n)
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        chars = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in chars:
+                chars.remove(s[l])
+                l += 1
+            chars.add(s[r])
+            res = max(res, r - l + 1) # corrected for indexing
+        return res
+            
 '''
-TLDR: Using sliding window + count dictionary, we can check and adjust (mostly our left pointer) if necessary and setting maxLen to our window size (r - l + 1 for index adjustment.
+TLDR: Using sliding window + count dictionary, we can check and adjust (mostly our left pointer) if necessary and setting maxLen to our window size (r - l + 1) for index adjustment.
 '''
                 
                     
