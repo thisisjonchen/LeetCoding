4b317082ce365bbc9b6eeaa805a768bd775faab2
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-07-17 00:37:12 -0400

    7/17/24

--- Problems/Two Pointers/15.py
+++ Problems/Two Pointers/15.py
@@ -1,4 +1,6 @@
 # LC 15. 3Sum
+
+# Solution with Hint - 6/25/24
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
@@ -27,6 +29,29 @@ class Solution:
                         l += 1
         return res
 
+# Retry 7/17/24
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        res = []
+        for x in range(len(nums)):
+            if nums[x] > 0:
+                break
+            if x > 0 and nums[x] == nums[x-1]:
+                continue
+            
+            l, r = x + 1, len(nums) - 1
+            while l < r:
+                target = nums[x] + nums[r] + nums[l]
+                if target > 0: r -= 1
+                elif target < 0: l += 1
+                else: 
+                    res.append([nums[x],nums[l],nums[r]])
+                    l += 1
+                    while l < r and nums[l] == nums[l-1]:
+                        l += 1
+                    r -= 1
+        return res
 '''
 TLDR: Fix one num and make it a two sum problem. To skip duplis, enforce "continues" and l+=1 at the "three-pointer" level and "two-pointer" level.
 '''
