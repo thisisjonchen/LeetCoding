a5f0238c9c1674e30b34668cb160c194c11ff977
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-07-16 22:09:54 -0400

    7/16/24

--- Problems/Sliding Window/643.py
+++ Problems/Sliding Window/643.py
@@ -1,4 +1,6 @@
 # LC 643. Maximum Average Subarray I
+
+# Attempted with Hint (5/19/24)
 class Solution:
     def findMaxAverage(self, nums: List[int], k: int) -> float:
         '''
@@ -22,6 +24,23 @@ class Solution:
 
         # Ultimately O(n+k)
         return max_sum / k
+
+# Retry 7/16/24
+class Solution:
+    def findMaxAverage(self, nums: List[int], k: int) -> float:
+        maxSum = 0
+        l, r = 0, 0
+        while r < k: # Can use sum(nums[:k])
+            maxSum += nums[r]
+            r += 1
+        currSum = maxSum
+        while r < len(nums):
+            currSum += nums[r] - nums[l]
+            l += 1
+            r += 1
+            maxSum = max(maxSum, currSum)
+        return maxSum / k
+        
 '''
 TLDR: Useful to use sum() with [:k], then sliding window L pointer is [i-k] and R will be [i]
 '''
