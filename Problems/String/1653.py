fa12bf2a0dcc13de562f32e7c598b71a5e9e7295
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-07-30 19:56:44 -0400

    7/30/24

---
+++ Problems/String/1653.py
@@ -0,0 +1,32 @@
+# LC 1653. Minimum Deletions to Make String Balanced
+
+# T-O(n), S-O(n)
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        stack = []
+        res = 0
+        for char in s:
+            if char == "b":
+                stack.append(char)
+            else:
+                if stack and stack[-1] == "b":
+                    stack.pop()
+                    res += 1
+        return res
+
+# Better (from Solutions - Cred: SonuDutta):
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        bCount = 0
+        minDeletions = 0
+        for char in s:
+            if char == 'a':
+                minDeletions = min(minDeletions + 1, bCount)
+            else:
+                bCount += 1   
+        return minDeletions
+
+'''
+TLDR: In better, essentially keeping track of min between "a" and "b". "b"s are incremented only for comparison, while "a"s are the condition for the comparison.
+'''
+
