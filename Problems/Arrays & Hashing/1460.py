db4cc511ec94f111663eb201fa4bbfb679894170
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-08-02 21:35:50 -0400

    8/3/24

--- Problems/Arrays & Hashing/1460.py
+++ Problems/Arrays & Hashing/1460.py
@@ -1,16 +1,15 @@
-# LC 1460. Make Two Arrays Equal by Reversing Subarrays | LC Daily - 8/3/24
+# LC 1460. Make Two Arrays Equal by Reversing Subarrays
 class Solution:
     def canBeEqual(self, target: List[int], arr: List[int]) -> bool:
-        freqTar = defaultdict(int)
-        for num in target:
-            freqTar[num] += 1
-        freqArr = defaultdict(int)
-        for num in arr:
-            freqArr[num] += 1
-        if freqArr == freqTar:
-            return True
-        return False
+        freqTar, freqArr = defaultdict(int), defaultdict(int)
+        for i in range(len(target)):
+            freqTar[target[i]] += 1
+            freqArr[arr[i]] += 1
+        return freqTar == freqArr
 
 '''
 TLDR: Count freq and compare
+
+TC O(n)
+SC O(n)
 '''
