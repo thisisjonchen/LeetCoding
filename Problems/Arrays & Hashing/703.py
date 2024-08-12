a05c7f5d54b75f8ef5a818991c58194a625cd601
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-08-12 09:29:19 -1000

    8/11/12

---
+++ Problems/Arrays & Hashing/703.py
@@ -0,0 +1,17 @@
+# LC 703. Kth Largest Element in a Stream | LC Daily 8/12/24
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.heap = sorted(nums)
+        self.kth = k
+
+    def add(self, val: int) -> int:
+        self.heap += [val]
+        self.heap.sort()
+        return self.heap[-self.kth]
+'''
+TLDR: Use heap
+
+TC O(nlogn)
+SC O(n)
+'''
