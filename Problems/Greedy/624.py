49e73160c3f4cce39e9abccb57b821407bda0ee0
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-08-16 17:44:20 -0400

    8/16/24

---
+++ Problems/Greedy/624.py
@@ -0,0 +1,18 @@
+# LC 624. Maximum Distance in Arrays | LC Daily - 8/16/24
+class Solution(object):
+    def maxDistance(self, arrays):
+        smallest = arrays[0][0]
+        biggest = arrays[0][-1]
+        max_distance = 0
+
+        for i in range(1, len(arrays)):
+            arr = arrays[i]
+            max_distance = max(max_distance, abs(arr[-1] - smallest), abs(biggest - arr[0]))
+            smallest = min(smallest, arr[0])
+            biggest = max(biggest, arr[-1])
+
+        return max_distance
+
+  '''
+  TLDR: Will revisit
+  '''
