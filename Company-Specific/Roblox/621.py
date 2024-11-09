409f158df8e844b43395a1bbaefd4f1751ecdfaa
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-11-09 10:31:11 -0500

    11/9/24

--- Company-Specific/Roblox/621.py
+++ Company-Specific/Roblox/621.py
@@ -5,7 +5,7 @@ class Solution:
     def leastInterval(self, tasks: List[str], n: int) -> int:
         # Formula: Idle_Time = Idle_Time - min(Max_freq - 1, Current_Freq)
         freq = Counter(tasks) # Counter is faster (in typing out) than using just defaultdict + loop
-        freq.sort(reverse = True)
+        freq = sorted(freq.values(), reverse = True)
         maxFreq = freq[0]
         idleTime = (maxFreq - 1) * n
         for f in freq[1:]:
@@ -13,7 +13,6 @@ class Solution:
         
         idleTime = max(0, idleTime)
         return len(tasks) + idleTime
-
 
 '''
 TLDR: Determine the idle time
