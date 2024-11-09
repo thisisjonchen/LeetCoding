5e2563bea513c02d1a75d8fb3891cd051c66454b
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-11-09 10:27:34 -0500

    11/9/24

--- Company-Specific/Roblox/48.py
+++ Company-Specific/Roblox/48.py
@@ -2,7 +2,7 @@
 class Solution:
     def rotate(self, matrix: List[List[int]]) -> None:
         """
-        Do not return anything, modify matrix in-place instead.
+        Do not return anything, modify matrix in-place instead
         """
         for i in range(len(matrix)):
             for j in range(i, len(matrix)):
