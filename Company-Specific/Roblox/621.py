9f02dd72e09c1246dd013e44daf2a666b5d5580e
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-11-09 10:29:42 -0500

    11/9/24

---
+++ Company-Specific/Roblox/621.py
@@ -0,0 +1,23 @@
+# LC 621. Task Scheduler
+
+# Solution from Solutions - Cred: Mayowa Daniel
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        # Formula: Idle_Time = Idle_Time - min(Max_freq - 1, Current_Freq)
+        freq = Counter(tasks) # Counter is faster (in typing out) than using just defaultdict + loop
+        freq.sort(reverse = True)
+        maxFreq = freq[0]
+        idleTime = (maxFreq - 1) * n
+        for f in freq[1:]:
+            idleTime -= min(maxFreq - 1, f) # Formula
+        
+        idleTime = max(0, idleTime)
+        return len(tasks) + idleTime
+
+
+'''
+TLDR: Determine the idle time
+
+TC O(nlogn)
+SC O(n)
+'''
