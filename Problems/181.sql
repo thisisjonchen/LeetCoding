ac65e0af806a555c54212a3a9434234f710c976d
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-11-29 17:52:04 -0500

    Delete Problems/181.sql

--- Problems/181.sql
+++
@@ -1,7 +0,0 @@
-# LC/DB 181. Employees Earning More Than Their Managers
-
-# e1 == manager, e2 == employee
-SELECT e2.name as Employee 
-FROM employee e1 
-INNER JOIN employee e2 ON e1.id = e2.managerID
-WHERE e1.salary < e2.salary
