6d144233ec31cad531ca6296f009c2eca15569b4
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2024-12-31 13:40:37 -0500

    Create 252.py

---
+++ Problems/Intervals/252.py
@@ -0,0 +1,34 @@
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        '''
+        intervals.sort() # sort intervals into order
+        # Check if any intervals are conflicting
+        if not intervals:
+            return True
+
+        earliestStart, latestEnd = intervals[0][0], intervals[0][1]
+        for i in range(1, len(intervals)):
+            currStart, currEnd = intervals[i][0], intervals[i][1]
+            if (currStart - latestEnd) < 0 or (currEnd - earliestStart) < 0:
+                return False
+            earliestStart = min(currStart, earliestStart)
+            latestEnd = max(currEnd, latestEnd)
+        return True
+        '''
+
+        # A more succinct solution is just checking if intervals[i-1][end] > intervals[i][start]'
+        # Again, no intervals should overlap.
+
+        intervals.sort(key=lambda i: i[0]) # can sort explicitly by the first index (start times)
+        for i in range(1, len(intervals)):
+            curr, prev = intervals[i], intervals[i-1]
+            if prev[1] > curr[0]:
+                return False
+        return True
+
+'''
+TLDR: Simply check end time of prev and start time of curr. If prev end is > curr start, then overlap == False. Else, if nothing, return True
+
+TC O(nlogn)
+SC O(1)
+'''
