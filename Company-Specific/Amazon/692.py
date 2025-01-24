fc249925ba5ce93d345131256ee617228499b0f1
Jon Chen <82542510+thisisjonchen@users.noreply.github.com>
2025-01-24 12:21:38 -0500

    1/24/25

---
+++ Company-Specific/Amazon/692.py
@@ -0,0 +1,24 @@
+# LC 692. Top K Frequent Words
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        freq = defaultdict(int)
+        for w in words:
+            freq[w] += 1
+
+        freq = sorted(freq.items(), key=lambda x: (-x[1], x[0])) # sort freq in descending order (with negative, largest freq is first) and THEN by alphabetical
+
+        res = []
+        for wp in freq:
+            w, f = wp
+            res.append(w)
+            k -= 1
+            if k == 0:
+                break 
+        return res
+
+'''
+TLDR: Gather freq of each word, sort by freq first in descending order, then by the alphabetical order (lexicographical)
+
+TC O(klogn)
+SC O(n)
+'''
